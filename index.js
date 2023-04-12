7d68c23d9aa235bdb7c884dd8286036c486c0a62
snpatel <sh732170@dal.ca>
2023-04-12 01:24:03 -0300

    timeout error

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
+// console.log(process.env.REACT_APP_FRONTEND);
+
 const userRouter = require("./routes/userRoutes");
 const blogRouter = require("./routes/blogRoutes");
 const globalErrorHandler = require("./controllers/errorController");
@@ -41,20 +43,22 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(
 //     cors({
-//         origin: "*",
+//         origin: [process.env.REACT_APP_FRONTEND],
 //         credentials: true,
 //         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
 //     })
 // );
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    console.log("INSIDE CORS");
+    res.header("Access-Control-Allow-Origin", "192.168.0.0/24");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
     if (req.method === "OPTIONS") {
         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
         return res.status(200).json({});
     }
+    console.log("HEY, THIS IS NOT THE CORS ISSUE");
     next();
 });
 
